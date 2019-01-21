c52c883e53ddd8b775fd4ab4023ecd36fab56127
HENRYHKll <46657690+HENRYHKll@users.noreply.github.com>
2019-01-21 12:37:50 +0300

    дз урок 3 уровень easy

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -4,8 +4,10 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
-def my_round(number, ndigits):
-    pass
+def my_round(number, ndigits): 
+	number = number*(10**ndigits)+0.41 
+	number = number//1 
+	return number/(10**ndigits) 
 
 
 print(my_round(2.1234567, 5))
@@ -20,9 +22,26 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+	tn = ticket_number
+	x = tn // 1000
+
+	one = x // 100
+	two = x // 10 % 10
+	three = x % 10
+
+	y = tn % 1000
+
+	four = y // 100
+	five = y // 10 % 10
+	six = y % 10
+
+	if (one + two + three) == (four + five + six):
+		return ("Это билет счастливый!))")
+	else:
+		return("Этому билету сегодня не повезло, не волнуйся завтра будет еще хуже (((")
+
 
 
 print(lucky_ticket(123006))
-print(lucky_ticket(12321))
+print(lucky_ticket(123210))
 print(lucky_ticket(436751))
