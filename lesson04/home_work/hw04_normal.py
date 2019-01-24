333a47feb6c3475fcd5a3a072c0888a2e0d14f59
HENRYHKll <46657690+HENRYHKll@users.noreply.github.com>
2019-01-24 14:44:08 +0300

    дз 4 урок уровень normal.py

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,8 +1,11 @@
+__author__ = 'Генрых'
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+
+import re
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -20,6 +23,16 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+# RE
+list_re = re.findall(r'[a-z]+', line)
+print('С RE: ', list_re)
+# No RE 
+print('---------------------------------------------------------------------')
+list_nore = []
+for i in line:
+       if i.islower():
+              list_nore.append(i)
+print('Без RE', list_nore)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +58,50 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+result_list = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print(result_list)
+print('--------------------------------------')
+result_list2 = []
+rem_i = 0
+for idx, ch in enumerate(line_2):
+     if ch.isupper() and line_2[idx - 1].islower() and line_2[idx - 2].islower():
+              i = idx
+              st = ''
+              while line_2[i].isupper():
+                     st += line_2[i]
+                     if i + 1 <= len(line_2) - 1:
+                           i += 1
+                     else:
+                            break
+              if len(st[:-2]) > 0:
+                     result_list2.append(st[:-2])
+print(result_list2)
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+import random
+import re
+
+def string_of_numbers():
+       seq_str = ''
+       for x in range(2500):
+              seq_str += str(random.randint(0, 9))
+       return seq_str
+
+file = open('hw4.txt', 'w', encoding='UTF-8')
+file.write(string_of_numbers())
+file.close()
+file = open('hw4.txt', 'r')
+seq_r = str(file.read())
+print('seq_r = ', seq_r)
+
+seq_s = re.findall(r'0{2,}|1{2,}|3{2,}|4{2,}|5{2,}|6{2,}|7{2,}|8{2,}|9{2,}', seq_r)
+print('seq_s = ', seq_s)
+
+seq_max = [i for i in seq_s if len(i) == len(max(seq_s, key=len))]
+print('seq_max =  ', seq_max)
